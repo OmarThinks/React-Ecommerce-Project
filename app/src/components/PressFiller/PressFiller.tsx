75e0d4f0477ec5f4957e6588ffedbed559c0ba28
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-03-08 17:28:11 +0200

    PressFiller: indexed

---
+++ app/src/components/PressFiller/PressFiller.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from "react";
+
+const PressFiller = memo(() => {
+  return <div>PressFiller</div>;
+});
+
+PressFiller.displayName = "PressFiller";
+
+export default PressFiller;
--- app/src/components/index.tsx
+++ app/src/components/index.tsx
@@ -1,8 +1,17 @@
 import Button from "./Button/Button";
 import CircleIcon from "./CircleIcon/CircleIcon";
 import ColorOption from "./ColorOption/ColorOption";
+import PressFiller from "./PressFiller/PressFiller";
 import ProductImage from "./ProductImage/ProductImage";
 import SizeOption from "./SizeOption/SizeOption";
 import Spacer from "./Spacer/Spacer";
 
-export { Button, CircleIcon, ColorOption, ProductImage, SizeOption, Spacer };
+export {
+  Button,
+  CircleIcon,
+  ColorOption,
+  PressFiller,
+  ProductImage,
+  SizeOption,
+  Spacer,
+};
