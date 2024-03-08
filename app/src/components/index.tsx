2d9b3101f89ac66e32eb21cc7a84fdfc7aeba941
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-03-08 14:24:23 +0200

    Button: indexed

--- app/src/components/index.tsx
+++ app/src/components/index.tsx
@@ -1,7 +1,8 @@
+import Button from "./Button/Button";
+import CircleIcon from "./CircleIcon/CircleIcon";
 import ColorOption from "./ColorOption/ColorOption";
 import ProductImage from "./ProductImage/ProductImage";
 import SizeOption from "./SizeOption/SizeOption";
 import Spacer from "./Spacer/Spacer";
-import CircleIcon from "./CircleIcon/CircleIcon";
 
-export { ColorOption, ProductImage, SizeOption, Spacer, CircleIcon };
+export { Button, CircleIcon, ColorOption, ProductImage, SizeOption, Spacer };
