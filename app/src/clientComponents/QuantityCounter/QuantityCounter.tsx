4c4fe2c581d9a07a249edbf15dc72d17695a07fd
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-03-08 17:16:56 +0200

    QuantityCounter: increment

--- app/src/clientComponents/QuantityCounter/QuantityCounter.tsx
+++ app/src/clientComponents/QuantityCounter/QuantityCounter.tsx
@@ -1,18 +1,37 @@
+"use client";
 import { CircleIcon } from "@/components";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
 const QuantityCounter = memo(() => {
+  const [counter, setCounter] = useState(1);
+
   return (
     <div
       className="w-[150px] flex flex-row justify-between p-1 rounded-full items-center"
       style={{ backgroundColor: "#eee" }}
     >
-      <CircleIcon Icon={FaMinus} size={20} bgColor="yellow" iconColor="black" />
+      <CircleIcon
+        Icon={FaMinus}
+        size={20}
+        bgColor="yellow"
+        iconColor="black"
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      />
       <p className="text-[13px] font-bold" style={{ color: "purple" }}>
-        1
+        {counter}
       </p>
-      <CircleIcon Icon={FaPlus} size={20} bgColor="yellow" iconColor="black" />
+      <CircleIcon
+        Icon={FaPlus}
+        size={20}
+        bgColor="yellow"
+        iconColor="black"
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      />
     </div>
   );
 });
--- app/src/components/CircleIcon/CircleIcon.tsx
+++ app/src/components/CircleIcon/CircleIcon.tsx
@@ -7,17 +7,24 @@ const CircleIcon = memo(
     size,
     bgColor,
     iconColor,
+    onClick,
   }: {
     Icon: IconType;
     size: number;
     bgColor: string;
     iconColor: string;
+    onClick?: () => void;
   }) => {
     return (
       <div
-        className="rounded-full overflow-hidden flex items-center justify-center"
+        className="rounded-full overflow-hidden flex items-center justify-center relative"
         style={{ width: size, height: size, backgroundColor: bgColor }}
       >
+        <button
+          style={{}}
+          className="w-full h-full absolute z-10"
+          onClick={onClick}
+        />
         <Icon size={size / 2} color={iconColor} />
       </div>
     );
