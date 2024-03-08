a71577b902b19871b5723a5cceb8926391196d29
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-03-08 21:33:08 +0200

    Cart: progress

--- app/src/clientComponents/Cart/Cart.tsx
+++ app/src/clientComponents/Cart/Cart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CircleIcon, PressFiller } from "@/components";
+import { Button, CircleIcon, PressFiller, ProductImage } from "@/components";
 import { memo, useCallback, useEffect, useState } from "react";
 import { FaCartShopping } from "react-icons/fa6";
 import { Drawer } from "@mui/material";
@@ -61,7 +61,14 @@ const Cart = memo(() => {
             //onClose={() => setIsDrawerOpen(false)}
             anchor="right"
           >
-            <EmptyDrawer setIsDrawerOpen={setIsDrawerOpen} />
+            {cartItemsNumber !== 0 ? (
+              <ItemsDrawer
+                setIsDrawerOpen={setIsDrawerOpen}
+                cartItemsNumber={cartItemsNumber}
+              />
+            ) : (
+              <EmptyDrawer setIsDrawerOpen={setIsDrawerOpen} />
+            )}
           </Drawer>
         </div>
         <FaCartShopping size={30} />
@@ -118,6 +125,89 @@ const EmptyDrawer = memo(
 );
 
 EmptyDrawer.displayName = "EmptyDrawer";
+
+const ItemsDrawer = memo(
+  ({
+    setIsDrawerOpen,
+    cartItemsNumber,
+  }: {
+    setIsDrawerOpen: (a: boolean) => void;
+    cartItemsNumber: number;
+  }) => {
+    return (
+      <div
+        className="w-[500px]"
+        style={{
+          width: 500,
+          display: "flex",
+          flexDirection: "column",
+          padding: 15,
+          gap: 20,
+        }}
+      >
+        <div className="self-end" style={{ alignSelf: "flex-end" }}>
+          <CircleIcon
+            Icon={FaTimes}
+            size={50}
+            bgColor="transparent"
+            iconColor="gray"
+            onClick={() => setIsDrawerOpen(false)}
+          />
+        </div>
+        <div
+          className="text-center"
+          style={{ fontSize: 20, fontWeight: "bold", color: "purple" }}
+        >
+          My cart
+        </div>
+        <div style={{ fontSize: 14, fontWeight: "600", color: "black" }}>
+          Cart Summary
+        </div>
+        <div
+          style={{
+            fontSize: 18,
+            fontWeight: "700",
+            color: "black",
+            textAlign: "center",
+          }}
+        >
+          Total: {cartItemsNumber * 9000} LE
+        </div>
+        <div
+          style={{
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: "lightgray",
+            padding: 10,
+            alignSelf: "stretch",
+            display: "flex",
+            flexDirection: "row",
+            gap: 15,
+          }}
+        >
+          <ProductImage />
+        </div>
+
+        <div className="flex flex-row justify-between gap-4">
+          <Button
+            style={{ flex: 1 }}
+            text={"Review Cart"}
+            bgColor="yellow"
+            textColor="black"
+          />
+          <Button
+            style={{ flex: 1 }}
+            text={"Complete Checkout"}
+            bgColor="purple"
+            textColor="white"
+          />
+        </div>
+      </div>
+    );
+  }
+);
+
+ItemsDrawer.displayName = "ItemsDrawer";
 
 Cart.displayName = "Cart";
 
